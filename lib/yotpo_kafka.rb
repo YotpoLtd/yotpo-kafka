13d017bb124aae1ab2b707a8a637d9ad63070d5f
Dana Tal <dtal@yotpo.com>
2018-12-11 16:13:30 +0200

    initial commit

---
+++ lib/yotpo_kafka.rb
@@ -0,0 +1,12 @@
+require 'yotpo_kafka/producer'
+require 'yotpo_kafka/producer_worker'
+require 'yotpo_kafka/consumer_worker'
+require 'yotpo_kafka/consumer'
+require 'yotpo_kafka/consumer_config'
+require 'yotpo_kafka/producer_config'
+require 'yotpo_kafka/consumer_runner'
+
+
+module YotpoKafka
+
+end
