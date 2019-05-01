cdfed52b5d6ab9c3d174eda5a9002c42b4e7c390
bronhaim <bronhaim@gmail.com>
2019-05-01 13:33:52 +0300

    fix conversion to int

--- lib/yotpo_kafka/consumer.rb
+++ lib/yotpo_kafka/consumer.rb
@@ -93,10 +93,12 @@ module YotpoKafka
 
         failure_topic = get_fail_topic_name(t)
         begin
+          log_info('Created new topic: ' + failure_topic,
+                   partitions_num: YotpoKafka.default_partitions_num,
+                   replication_factor: YotpoKafka.default_replication_factor)
           YotpoKafka.kafka.create_topic(failure_topic,
-                                        num_partitions: YotpoKafka.default_partitions_num,
-                                        replication_factor: YotpoKafka.default_replication_factor)
-          log_info('Created new topic: ' + failure_topic)
+                                        num_partitions: YotpoKafka.default_partitions_num.to_i,
+                                        replication_factor: YotpoKafka.default_replication_factor.to_i)
         rescue Kafka::TopicAlreadyExists
           nil
         end
