9b5d4234e8d877ba7d8b31d5b7a6fd29700a12ee
Gal Israeli <gisraeli@yotpo.com>
2019-05-21 10:35:25 +0300

    fix(consumer, producer):convert to json fallbacks

--- lib/yotpo_kafka.rb
+++ lib/yotpo_kafka.rb
@@ -8,7 +8,7 @@ module YotpoKafka
   class << self; attr_accessor :kafka; end
   class << self; attr_accessor :retry_topic; end
   class << self; attr_accessor :fatal_topic; end
-  class << self; attr_accessor :include_headers; end
+  class << self; attr_accessor :kafka_v2; end
   class << self; attr_accessor :retry_header_key; end
   class << self; attr_accessor :default_partitions_num; end
   class << self; attr_accessor :default_replication_factor; end
@@ -19,6 +19,6 @@ YotpoKafka.kafka = Kafka.new(YotpoKafka.seed_brokers) unless YotpoKafka.kafka
 YotpoKafka.retry_header_key = 'retry'
 YotpoKafka.retry_topic = 'retry_handler'
 YotpoKafka.fatal_topic = 'fatal'
-YotpoKafka.include_headers = Kafka::FetchedMessage.instance_methods.include?(:headers)
+YotpoKafka.kafka_v2 = Kafka::FetchedMessage.instance_methods.include?(:headers)
 YotpoKafka.default_partitions_num = ENV['DEFAULT_PARTITIONS_NUM'] || 1
 YotpoKafka.default_replication_factor = ENV['DEFAULT_REPLICATION_FACTOR'] || 1
