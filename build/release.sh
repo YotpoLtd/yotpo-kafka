c88984123e5853b1134804317f9f257e9e724f15
eladmaymon <emaymon@yotpo.com>
2020-01-14 15:32:30 +0200

    fix(travis): tag releases

--- build/release.sh
+++ build/release.sh
@@ -13,6 +13,12 @@ curl -u$JFROG_USER:$JFROG_PASSWORD $RUBYGEMS_HOST/api/v1/api_key.yaml > ~/.gem/c
 chmod 0600 ~/.gem/credentials
 
 if [[ $TRAVIS_BRANCH == "master" ]]; then
+  echo "Tagging the release branch"
+  git config user.email "travis-ci@yotpo.com"
+  git config user.name "Travis CI"
+  git tag -a "v-${BUILD_DATE}" -m "Committed by Travis-CI ${TRAVIS_BUILD_NUMBER} [ci skip]"
+  git push --tags
+
   GEM_FILE=$(find ./pkg -name *.gem)
     if [ -z "$GEM_FILE" ]; then
         echo "Didn't find any suitable gem to release."
